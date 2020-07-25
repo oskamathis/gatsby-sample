24166f2b78261c354690b8bfe8286106bc532872
OskaMathis <oskamathis@gmail.com>
2020-07-25 19:05:02 +0900

    [add]記事のベースパスとサイトメタデータを設定

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,22 +2,24 @@ module.exports = {
   plugins: [
     {
       resolve: `gatsby-theme-blog`,
-      options: {},
+      options: {
+        basePath: `/posts`,
+      },
     },
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `Tech Blog`,
+    author: `OskaMathis`,
+    description: `徒然なるままに`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/oskamathis`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/OskaMathis`,
       },
     ],
   },
