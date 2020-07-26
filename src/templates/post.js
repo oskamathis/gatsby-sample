8b8aa676cfc08d176106c16b5bd9fcd68dd543c9
OskaMathis <oskamathis@gmail.com>
2020-07-27 04:41:39 +0900

    [update]記事のレイアウトを調整

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,32 +2,39 @@ import React from "react";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+
 const Post = ({ data }) => {
-  const { title, body, tags } = data.contentfulPost;
+  const { title, date, body, tags } = data.contentfulPost;
   return (
     <Layout>
       <SEO title={title} />
       <div className="post">
         <h1>{title}</h1>
-        <h4 className="tags">
-          {tags.map(tag => (
-            <span className="tag" key={tag}>
-              {tag.title}
-            </span>
-          ))}
-        </h4>
+        <div className="post-metadata" >
+          <span>{date}</span>
+          <span className="tags">
+            {tags.map(tag => (
+              <span className="tag" key={tag}>
+                {`・`}{tag.title}
+              </span>
+            ))}
+          </span>
+        </div>
+
         <div dangerouslySetInnerHTML={{ __html: body.childMarkdownRemark.html }} />
         <Link to="/">Back to Home</Link>
       </div>
     </Layout>
   );
 };
+
 export default Post;
 export const pageQuery = graphql`
   query($slug: String!) {
     contentfulPost(slug: { eq: $slug }) {
       title
       slug
+      date(formatString: "YYYY-MM-DD")
       body {
         childMarkdownRemark {
           html
