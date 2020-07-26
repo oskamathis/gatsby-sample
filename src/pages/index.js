4493b8f1399a1722ccf8df6872c4a276d9b5d9fc
OskaMathis <oskamathis@gmail.com>
2020-07-27 04:41:07 +0900

    [add]トップページに記事一覧を追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,38 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const IndexPage = ({ data }) => {
+  const { edges } = data.allContentfulPost;
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {edges.map(edge => (
+        <div className="post" key={edge}>
+          <Link to={"/posts/"+edge.node.slug}>{edge.node.title}</Link>
+        </div>
+      ))}
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+    </Layout>
+  );
+};
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
-
-export default IndexPage
+export default IndexPage;
+export const postsQuery = graphql`
+  query {
+    allContentfulPost {
+      edges {
+        node {
+          id
+          slug
+          title
+          date
+        }
+      }
+    }
+  }
+`;
