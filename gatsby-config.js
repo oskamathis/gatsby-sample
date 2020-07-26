842d2d50208e12d0ff93ad0edede1c7548aafd2a
OskaMathis <oskamathis@gmail.com>
2020-07-26 14:03:53 +0900

    Initial commit from gatsby: (https://github.com/gatsbyjs/gatsby-starter-default.git)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,34 @@
-const dotenv = require("dotenv")
-
-if (process.env.ENVIRONMENT !== "production") {
-  dotenv.config()
-}
-
 module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-theme-blog`,
-      options: {
-        basePath: `/posts`,
-      },
-    },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
-    },
-  ],
-  // Customize your site metadata:
   siteMetadata: {
-    title: `Tech Blog`,
-    author: `OskaMathis`,
-    description: `徒然なるままに`,
-    social: [
-      {
-        name: `twitter`,
-        url: `https://twitter.com/oskamathis`,
-      },
-      {
-        name: `github`,
-        url: `https://github.com/OskaMathis`,
-      },
-    ],
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 }
